8799cc17650f86d76e4c40d30aa978eab879d669
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-06 17:32:20 -0300

    integracion footer y navbar

--- Capitulos/models.py
+++ Capitulos/models.py
@@ -6,6 +6,7 @@ class Capitulos(models.Model):
     title = models.CharField(max_length=100)
     video = models.FileField(upload_to='videos')
     text = models.TextField(max_length=300)
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.title   
--- Manga/models.py
+++ Manga/models.py
@@ -5,6 +5,7 @@ class Manga(models.Model):
     title = models.CharField(max_length=100)
     img = models.ImageField()
     text = models.TextField(max_length=300)
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.title
