a0b1cd3703693215884d6371ae54453acfa41ec4
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-06 12:47:01 -0300

    integracion de models.Manga

--- Manga/admin.py
+++ Manga/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Capitulos
+from .models import Manga
 # Register your models here.
-admin.site.register(Capitulos)
+admin.site.register(Manga)
--- Manga/models.py
+++ Manga/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-
 # Create your models here.
-class manga(models.Model):
+class Manga(models.Model):
     
     title = models.CharField(max_length=100)
     img = models.ImageField()
     text = models.TextField(max_length=300)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 #pendiente migraciones
