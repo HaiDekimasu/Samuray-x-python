47d4127e835aef103063a5e01c33fb451eccfd70
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-06 12:21:23 -0300

    modified:   Manga/models.py

--- Manga/models.py
+++ Manga/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class manga(models.Model):
+    
+    title = models.CharField(max_length=100)
+    img = models.ImageField()
+    text = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+#pendiente migraciones
