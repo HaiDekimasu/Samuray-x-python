8a544e2da1e9c7f52fc7d4e163fa93ede70a97da
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-08 00:06:47 -0300

    modified:   db.sqlite3

--- Capitulos/models.py
+++ Capitulos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Capitulos(models.Model):
     title = models.CharField(max_length=100)
-    video = models.FileField(upload_to='videos')
+    video = models.FileField(upload_to='videos/%y')
     text = models.TextField(max_length=300)
     id = models.AutoField(primary_key=True)
 
