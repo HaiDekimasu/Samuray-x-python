cd7f52322b27017e72dfb0e4e1bd7b88c545702e
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-08 16:25:59 -0300

    modified:   Manga/views.py

--- Manga/views.py
+++ Manga/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Manga
 
 # Create your views here.
-def Manga(request):
+def manga(request):
     manga = Manga.objects.all()
-    return render(request, 'manga.html',{'manga': manga})
+    return render(request, 'manga.html', {'manga': manga})
